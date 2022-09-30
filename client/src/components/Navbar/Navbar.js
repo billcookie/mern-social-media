15bde61104c6fd1a1324924c27871fc4de8bf1e0
Bill Cook <billcook8122@gmail.com>
2022-09-30 21:52:38 +0900

    navbar and home setup. refactoring. Next auth

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import Link from 'react-router-dom';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
+import useStyles from "./styles";
+import memories from '../../images/memories.png'
 
 const Navbar = () => {
 
@@ -11,11 +13,24 @@ const Navbar = () => {
   return (
     <div>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography component = {Link} to= "/" className={classes.heading} variant="h2" align="center">Memories</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
+        <Toolbar className={classes.toolbar}>
+          {user ? (
+            <div>
+              <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+              <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+              <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+            </div>
+          ) : (
+            <Button component={Link} to="/auth" variant="contained" color="primary">Sign in</Button>
+          )
+          }
+        </Toolbar>
       </AppBar>
-
     </div>
+
+
 
   )
 }
