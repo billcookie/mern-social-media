22924d6eab9429cd0c58760153ca25eb996e302a
Bill Cook <billcook8122@gmail.com>
2022-10-05 10:41:32 +0900

    signup controller with jwt, bcrypt implentation

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 
 import jwt from 'jsonwebtoken';
+import user from '../models/user.js';
 
 import User from "../models/user.js";
 
@@ -24,9 +25,29 @@ export const signin = async (req, res) => {
 
 }
 
-export const signup = async (rep, res) => {
+export const signup = async (req, res) => {
+  const {email, password, confirmPassword, firstName, lastName} = req.body;
 
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser) return res.status(400).json({ message: "User already exists" });
+
+    if(password !== confirmPassword) return res.status(400).json({ message: "Passwords do not match"});
+    // check syntax
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await user.create( {email, password: hashedPassword, name:`${firstName} ${lastName}`} );
+
+    const token = jwt.sign({ email: result.email, id:result._id}, 'test', { expiresIn: '1h'});
+
+    res.status(200).json({ result, token});
+
+  } catch (error) {
+    res.status(500).json({message: "Something went wrong."});
+  }
 }
 
 
-// this code can be used for other apps.
+// this code can be used for other apps. Review this code.
