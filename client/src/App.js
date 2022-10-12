99c41587bc2f34ffb0975df8201dcd1d65c52e57
Bill Cook <billcook8122@gmail.com>
2022-10-12 15:41:18 +0900

    added slight changes to pagination.

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
-import { GoogleOAuthProvider } from "react-google-login";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/auth" element={<Auth/>} />
-      </Routes>
-    </Container>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
 
   );
